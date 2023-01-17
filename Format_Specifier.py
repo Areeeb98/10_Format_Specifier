cb32398516eb535c6e14955c3a2604d71a8c7342
Areeeb98 <122514572+Areeeb98@users.noreply.github.com>
2023-01-17 19:35:00 +0530

    Add files via upload

---
+++ Format_Specifier.py
@@ -0,0 +1,59 @@
+"""
+Day: Tuesday
+Date: 17th Jan 2023
+Time: 7 PM
+Topic: Format specifier
+Author: Areeb Ahmed
+"""
+
+
+a=7
+# print(a,type(a))
+# print("The value of a is",a)
+# print("The value of a is %d"%a)
+# print("The value of a is %f"%a)
+# print("The value of a is %s"%a)
+
+# b=9.5
+# print(b,type(b))
+# print("The value of a is",b)
+# print("The value of a is %d"%b)
+# print("The value of a is %f"%b)
+# print("The value of a is %s"%b)
+
+# c=2.47
+# print("The value of c is %.1f" %c)
+# print("The value of c is %s"%c)
+
+d=2
+# print("The value of d is %f"%d)
+
+g=1.3+4.5
+# print("%f"%g)
+# print("%.1f"%g)
+# print("%2f"%g)
+# print("%.3f"%g)
+# print("%7f"%g)
+# print("%8f"%g)
+# print("%9f"%g)
+# print("%10f"%g)
+# print("%11f"%g)
+
+h=0.2+0.7
+y=0.2+0.4
+k=0.2+0.1
+# print(h)
+# print(y)
+# print(k)
+# print("The value of h is %.1f"%h)
+# print("The value of y is %.1f"%y)
+# print("The value of k is %.1f"%k)
+
+j="Josh"
+"""print("%d" %j) # when there is str in value format specifiers as int it won't work"""
+"""print("%f" %j) # when there is str in value format specifiers as float it won't work"""
+
+# i="Areeb"
+# print("My name is %s"%i)
+# print("My name is%s"%i)
+# print("My name is",i)
